9bb69a06dffa6c576cda528702fcf52bc79e3210
Sandra Rosch <sandra.rosch@uzh.ch>
2022-04-11 10:25:46 +0200

    a

---
+++ src/main/java/com/example/annotationwebsocket/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.example.annotationwebsocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        System.out.println("new connection");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        System.out.println("new connection1");
+
+        registry.addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins("*").withSockJS();
+    }
+
+}
---
+++ src/test/java/com/example/annotationwebsocket/AnnotationWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annotationwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
